603506fbcfd37d9e74fc285d66fee3335831371b
Relll06HUB <darrell.traveller@gmail.com>
2024-11-16 15:43:14 +0700

    Initial commit

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ojekku - Selalu Ada di Setiap Langkahmu</title>
+    <link rel="shortcut icon" href="./images/logo.png" type="image/x-icon">
+    <link rel="stylesheet" href="index.css">
+</head>
+<body>
+    <div class="container">
+        <div class="welcome">
+            <h1>Welcome</h1>
+        </div>
+        <div class="description">
+            <p> Please sign up for an account or login to an existing account </p>
+        </div>
+        <div>
+            <button class="btn1"><a href="./php/both_register.php">Register</a></button>
+        </div>
+        <div class="btn2">
+        <button class="btn2"><a href="./php/both_login.php">Login</a></button>
+        </div>
+    </div>
+</body>
+</html>
