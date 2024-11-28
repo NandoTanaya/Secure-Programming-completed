a913a21992931e55a712193998c52a7014324897
Nando Tanaya <68413740+NandoTanaya@users.noreply.github.com>
2024-11-29 00:03:58 +0700

    yg baru

--- database/ojekku.sql
+++ database/ojekku.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost
--- Generation Time: Nov 16, 2024 at 07:45 AM
+-- Generation Time: Nov 28, 2024 at 06:02 PM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -42,7 +42,11 @@ CREATE TABLE `drivers` (
 --
 
 INSERT INTO `drivers` (`id`, `name`, `email`, `phone`, `available`, `location`, `password`) VALUES
-(1, 'driver', 'driver@gmail.com', NULL, 1, NULL, '$2y$10$TJFbNfvAyWxY65yUAfNuaOVQ6tTnKuBT7sSTEzlvl3eXzMa0L1b.O');
+(1, 'driver', 'driver@gmail.com', NULL, 1, NULL, '$2y$10$TJFbNfvAyWxY65yUAfNuaOVQ6tTnKuBT7sSTEzlvl3eXzMa0L1b.O'),
+(2, '', 'driverku@gmail.com', NULL, 1, NULL, '$2y$10$uwRI0z6Q5JUzoTQ0fZzxgO6w9gMP.6kpCwhJ4CVSZFSvYmpu7PVDW'),
+(3, '', 'driverku2@gmail.com', NULL, 1, NULL, '$2y$10$rDwd5uORQXk678zUsMhhouUPDFileDIyFdkLyVCyPsDM2LIb1xfsC'),
+(4, 'driverku3', 'driverku3@gmail.com', NULL, 1, NULL, '$2y$10$3FULnNncjqAP7Ik/Q8XisO58CNT8D6sPzJrdtI8ePmSgFX977zl4C'),
+(5, 'driverku4', 'driverku4@gmail.com', NULL, 1, NULL, '$2y$10$mMnij7VsjMz4iAegl55cweOweeyGdGRHGYjIgJDi8Hy1GhI9RyFZS');
 
 -- --------------------------------------------------------
 
@@ -97,22 +101,26 @@ CREATE TABLE `users` (
   `id` int(11) NOT NULL,
   `username` varchar(128) NOT NULL,
   `email` varchar(255) NOT NULL,
-  `password` varchar(255) NOT NULL
+  `password` varchar(255) NOT NULL,
+  `reset_token_hash` varchar(64) DEFAULT NULL,
+  `reset_token_expires_at` datetime DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `username`, `email`, `password`) VALUES
-(1, 'www', 'der@example.com', '$2y$10$u96H1vwhbLWQtD1xkWnCOulgUwRbhb9b3Oq9M8kEYgw4T8nliznJS'),
-(2, 'darr', 'darr@example.com', '$2y$10$apB5h62LwH1Z9sohDVtjxel5qoSTjj6CwEIL8GMaCHnKyoDC/AIwu'),
-(14, 'halo', 'halo@gmail.com', '$2y$10$V9POzk0lcf3NBB5R7l1.zerY3oL5kKbKEcOqME4OvhiILqNcYMC3S'),
-(15, 'woi', 'woi@gmail.com', '$2y$10$Rj/Itfshuw38Wzcc3COVUOzBG8zGAMCSH2BIWQb3ybR9YPA67mAn.'),
-(16, 'woi1', 'woi1@gmail.com', '$2y$10$QLYIeBq48jucj1PtMrcrXe35vmqMmJGBDQsoLyZz9HU7tDM1FmKNq'),
-(17, 'test', 'test@gmail.com', '$2y$10$GRk373WzxIgsBvhEuJOHxOcNNgKx/ajqTL5k24AoEEL2uWDyAvdBa'),
-(18, 'testya', 'testya@email.com', '$2y$10$ahRFIpevzUK6K7exQ.J20evXoStavMqR5IE1q0KYh/rh78f1Idztq'),
-(20, 'testya1', 'testya1@email.com', '$2y$10$nrCc.rnowC92nDXKgcF1seWLkEW6FR1TAdyMqtkjZKF6wrBuJr5gC');
+INSERT INTO `users` (`id`, `username`, `email`, `password`, `reset_token_hash`, `reset_token_expires_at`) VALUES
+(1, 'www', 'der@example.com', '$2y$10$u96H1vwhbLWQtD1xkWnCOulgUwRbhb9b3Oq9M8kEYgw4T8nliznJS', NULL, NULL),
+(2, 'darr', 'darr@example.com', '$2y$10$apB5h62LwH1Z9sohDVtjxel5qoSTjj6CwEIL8GMaCHnKyoDC/AIwu', NULL, NULL),
+(14, 'halo', 'halo@gmail.com', '$2y$10$V9POzk0lcf3NBB5R7l1.zerY3oL5kKbKEcOqME4OvhiILqNcYMC3S', NULL, NULL),
+(15, 'woi', 'woi@gmail.com', '$2y$10$Rj/Itfshuw38Wzcc3COVUOzBG8zGAMCSH2BIWQb3ybR9YPA67mAn.', NULL, NULL),
+(16, 'woi1', 'woi1@gmail.com', '$2y$10$QLYIeBq48jucj1PtMrcrXe35vmqMmJGBDQsoLyZz9HU7tDM1FmKNq', NULL, NULL),
+(17, 'test', 'test@gmail.com', '$2y$10$GRk373WzxIgsBvhEuJOHxOcNNgKx/ajqTL5k24AoEEL2uWDyAvdBa', NULL, NULL),
+(18, 'testya', 'testya@email.com', '$2y$10$ahRFIpevzUK6K7exQ.J20evXoStavMqR5IE1q0KYh/rh78f1Idztq', NULL, NULL),
+(21, '&lt;script&gt;alert(&#039;XSS Test&#039;)&lt;/script&gt;', 'testaja@gmail.com', '$2y$10$dqHotYB8bJwNHV8ef.qzp.DNGkNuzdfKn3YKLt7VXOY0elaa0vZGq', NULL, NULL),
+(22, '&lt;script&gt;document.body.innerHTML = &#039;Hacked by XSS&#039;;&lt;/script&gt;', 'testaja2@gmail.com', '$2y$10$q6JmuyBdbZU7AIzGbA9co.IdwrTQ8AeNTXJFtfOR3YyxBWq0Cg3N.', NULL, NULL),
+(23, '&quot;&gt;&lt;img src=x onerror=alert(&#039;XSS&#039;)&gt;', 'testaja3@gmail.com', '$2y$10$IR5tMymkRok91Z/iBH3QuuD9oQHvjY5qEQgnyKVPnAv2Q849jTqsm', NULL, NULL);
 
 --
 -- Indexes for dumped tables
@@ -153,7 +161,7 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `drivers`
 --
 ALTER TABLE `drivers`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- AUTO_INCREMENT for table `orders`
@@ -171,7 +179,7 @@ ALTER TABLE `perjalanan`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
 
 --
 -- Constraints for dumped tables
